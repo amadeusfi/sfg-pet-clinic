a8e280d570efbca7ac478b2bd8c678365a4ace2f
Amadeus <figueiredo@tesolva.de>
2022-02-24 10:50:39 +0100

    List all Vets on Vet index page . Closes #21

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -45,9 +45,9 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
-        vet1.setFirstName("Vet2");
-        vet1.setLastName("Gateira");
+        vet2.setId(2L);
+        vet2.setFirstName("Vet2");
+        vet2.setLastName("Gateira");
 
         vetService.save(vet2);
 
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    //implement service
+    private final VetService vetService;
+    // inject with controller, dont need @Autowired because is a controller
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/vets/index","/vets/index.html"})
+    // bring model in controller
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
