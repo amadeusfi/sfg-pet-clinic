9a42fb76b38bf52707d557bebecf9b084beb3f2b
Amadeus <figueiredo@tesolva.de>
2022-02-24 15:30:21 +0100

    Create Vet Speciality Entity, Associate to Vet. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Specialty> specilities;
+
+    public Set<Specialty> getSpecilities() {
+        return specilities;
+    }
+
+    public void setSpecilities(Set<Specialty> specilities) {
+        this.specilities = specilities;
+    }
 }
